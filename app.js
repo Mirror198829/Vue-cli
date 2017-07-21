03c9fccb5c69268968e624be291aa1df2b5f5173
Mirror198829 <412366756@qq.com>
2017-07-21 22:41:45 +0800

    增加server

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-var express = require('express'); 
-var Mock=require('mockjs'); 
-var app = express();
+let express = require('express'); 
+let Mock=require('mockjs'); 
+let app = express();
 
-app.get('getName', function (req, res) { 
+app.get('/getData', function (req, res) { 
 	res.header("Access-Control-Allow-Origin", "*"); //设置跨域访问 
 	res.json(Mock.mock({ 
 		"status": 200, 
 		"data|1-9": [{ "name|5-8": /[a-zA-Z]/, "id|+1": 1, "value|0-500": 20 }] 
 	})); 
+});
+let server=app.listen('3000',function(){
+	let host=server.address().address;
+	let port=server.address().port;
+	console.log('Mock listening at http://localhost:'+port); 
 });
